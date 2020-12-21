a11cb570adc840380b6f6d351259179abcfaa4af
GFernando97 <angr7991@hotmail.com>
2020-12-21 19:24:42 +0100

    P4 - Flat & Smooth light

---
+++ Practica-Personal/Práctica_4/objetos_B3.h
@@ -0,0 +1,295 @@
+//**************************************************************************
+// Práctica 1 usando objetos
+//**************************************************************************
+
+#include <vector>
+#include <GL/gl.h>
+#include "vertex.h"
+#include <stdlib.h>
+#include <random>
+
+
+const float AXIS_SIZE=5000;
+typedef enum{COLORPICKER, NO_OPTION, UNIQUECOLOR} _opcion;
+typedef enum{POINTS,EDGES,SOLID_CHESS,SOLID, SOLID_ILUMINATED_FLAT, SOLID_ILUMINATED_GOURAUD} _modo;
+typedef enum{CUBO, PIRAMIDE, OBJETO_PLY, ESFERA, SEMIESFERA, CONO, CILINDRO, GENERICO, ARTICULADO} _tipo_objeto;
+
+//*************************************************************************
+// clase punto
+//*************************************************************************
+
+class _puntos3D
+{
+public:
+
+  
+  _puntos3D();
+void  draw_puntos(int grosor);
+void  colorPicker();
+
+vector<_vertex3f> vertices;
+_vertex3f color1{0.258,0.0,0.223};
+_vertex3f color2{0.862,0.8,1.0};
+
+};
+
+//*************************************************************************
+// clase triángulo
+//*************************************************************************
+
+class _triangulos3D: public _puntos3D
+{
+public:
+
+  _triangulos3D();
+void  draw_aristas(int grosor);
+void  draw_solido();
+void  draw_solido_ajedrez();
+void  draw_iluminacion_plana();
+void  draw_iluminacion_suave();
+void  draw(_modo modo, float grosor, _opcion &x);
+
+void  calcular_normales_caras();
+void  calcular_normales_vertices();
+
+vector<_vertex3i> caras;
+
+vector<_vertex3f> normales_caras;
+vector<_vertex3f> normales_vertices;
+
+
+bool b_normales_caras;
+bool b_normales_vertices;
+
+_vertex4f ambiente_difusa;
+_vertex4f especular;
+
+
+float brillo;
+};
+
+
+//*************************************************************************
+// clase objeto ply
+//*************************************************************************
+
+class _objeto_ply: public _triangulos3D
+{
+public:
+   _objeto_ply();
+
+int   parametros(char *archivo);
+};
+
+//************************************************************************
+// objeto por revolución
+//************************************************************************
+
+class _rotacion: public _triangulos3D
+{
+public:
+       _rotacion();
+void  parametros(vector<_vertex3f> perfil1, int num1, _tipo_objeto tipo, bool tapa_inferior, bool tapa_superior);
+
+vector<_vertex3f> perfil; 
+int num;
+};
+
+
+//*************************************************************************
+// clase cubo
+//*************************************************************************
+
+class _cubo: public _triangulos3D
+{
+public:
+
+  _cubo(float tam=0.5);
+};
+
+
+//*************************************************************************
+// clase piramide
+//*************************************************************************
+
+class _piramide: public _triangulos3D
+{
+public:
+
+  _piramide(float tam=0.5, float al=0.75);
+};
+
+
+class _esfera: public _rotacion{
+
+public:
+
+_esfera(float radio=2.0, int n_vert=20, int n_inst=30);
+
+};
+
+
+class _cono: public _rotacion{
+
+public:
+  _cono(float radio=2.0, int n_rev=30, int altura=2);
+};
+
+
+class _cilindro: public _rotacion{
+public:
+  _cilindro(float radio=2.0, int n_rev=20, int altura=2);
+};
+
+class _copa: public _rotacion{
+public: 
+  _copa();
+};
+
+class _semiesfera: public _rotacion{
+public:
+  _semiesfera(float radio=2.0, int n_vert=20, int n_inst=30);
+};
+
+
+
+
+//************************************************************************
+// objeto articulado: tanque
+//************************************************************************
+
+class _chasis: public _triangulos3D
+{
+public:
+       _chasis();
+void  draw(_modo modo, float grosor, _opcion &x);
+
+float altura;
+
+protected:
+_rotacion  rodamiento;
+_cubo  base;
+};
+
+//************************************************************************
+
+class _torreta: public _triangulos3D
+{
+public:
+       _torreta();
+void  draw(_modo modo,  float grosor,  _opcion &x);
+
+float altura;
+float anchura;
+
+protected:
+_cubo  base;
+_piramide parte_trasera;
+};
+
+//************************************************************************
+
+class _tubo: public _triangulos3D
+{
+public:
+       _tubo();
+void  draw(_modo modo,  float grosor,  _opcion &x);
+
+protected:
+_rotacion tubo_abierto; // caña del cañón
+};
+
+//************************************************************************
+
+class _tanque: public _triangulos3D
+{
+public:
+       _tanque();
+void  draw(_modo modo, float grosor,  _opcion &x);
+
+float giro_tubo;
+float giro_torreta;
+
+float giro_tubo_min;
+float giro_tubo_max;
+
+protected:
+_chasis  chasis;
+_torreta  torreta;
+_tubo     tubo;
+};
+
+
+//************************************************************************
+// objeto articulado: Robot
+//************************************************************************
+
+
+class _cabezaR:public _triangulos3D{
+public:
+  _cabezaR();
+  void draw(_modo modo, float grosor, _opcion &x );
+
+protected:
+  _semiesfera cabeza;
+  _esfera ojo_derecho;
+  _esfera ojo_izquierdo;
+  _cilindro oreja_izquierda;
+  _cilindro oreja_derecha;
+
+};
+
+class _torsoR:public _triangulos3D{
+public:
+  _torsoR();
+  void draw(_modo modo, float grosor, _opcion &x);
+protected:
+  _esfera torso;
+};
+
+class _brazoR:public _triangulos3D{
+public:
+  _brazoR();
+  void draw(_modo modo, float grosor, _opcion &x);
+
+protected:
+  _rotacion brazo;
+  _esfera mano;
+
+};
+
+class _piernaR:public _triangulos3D{
+public:
+  _piernaR();
+  void draw(_modo modo, float grosor, _opcion &x);
+protected:
+  _cilindro pierna;
+  _esfera pie;
+  _esfera dedo1;
+  _esfera dedo2;
+  _esfera dedo3;
+};
+
+
+class _robot:public _triangulos3D{
+public:
+  _robot();
+  void draw(_modo modo, float grosor, _opcion &x);
+
+  float giro_cabeza;
+
+  float giro_mano_izquierda;
+  float giro_mano_derecha;
+
+  float giro_pie_izquierdo;
+  float giro_pie_derecho;
+
+protected:
+  _cabezaR cabeza;
+  _torsoR torso;
+  _brazoR brazoDerecho;
+  _brazoR brazoIzquierdo;
+  _piernaR piernaDerecha;
+  _piernaR piernaIzquierda;
+};
+
---
+++ Practica-Personal/Práctica_4/practica_objetos_B3.cc
@@ -0,0 +1,387 @@
+//**************************************************************************
+// Práctica 1 usando objetos
+//**************************************************************************
+
+#include <GL/glut.h>
+#include <ctype.h>
+#include <math.h>
+#include <vector>
+#include "objetos_B3.h"
+
+
+using namespace std;
+
+//Variables Teclado
+_opcion opcion=NO_OPTION;
+_tipo_objeto t_objeto=CUBO;
+_modo   modo=POINTS;
+// variables que definen la posicion de la camara en coordenadas polares
+GLfloat Observer_distance;
+GLfloat Observer_angle_x;
+GLfloat Observer_angle_y;
+
+// variables que controlan la ventana y la transformacion de perspectiva
+GLfloat Size_x,Size_y,Front_plane,Back_plane;
+
+// variables que determninan la posicion y tamaño de la ventana X
+int Window_x=50,Window_y=50,Window_width=450,Window_high=450;
+
+// variables que determinan la velocidad de las modificaciónes del modelo jerárquico
+int v_giro_cabeza = 5;
+int v_giro_brazos = 5;
+int v_giro_piernas = 5;
+
+// objetos
+_cubo cubo;
+_piramide piramide;
+_objeto_ply  ply; 
+_rotacion rotacion; 
+_esfera esfera;
+_cilindro cilindro;
+_cono cono;
+_copa copa;
+_tanque tanque;
+_robot robot;
+//Ejemplos: Borrar despues
+_semiesfera semiesfera;
+_cabezaR cabeza;
+_brazoR brazo;
+_piernaR pierna;
+// _objeto_ply *ply1;
+
+ float alfa=0.0;
+ float beta=0.0;
+
+
+//**************************************************************************
+//
+//***************************************************************************
+
+void clean_window()
+{
+
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+}
+
+
+//**************************************************************************
+// Funcion para definir la transformación de proyeccion
+//***************************************************************************
+
+void change_projection()
+{
+
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+
+// formato(x_minimo,x_maximo, y_minimo, y_maximo,plano_delantero, plano_traser)
+//  plano_delantero>0  plano_trasero>PlanoDelantero)
+glFrustum(-Size_x,Size_x,-Size_y,Size_y,Front_plane,Back_plane);
+}
+
+//**************************************************************************
+// Funcion para definir la transformación*ply1 de vista (posicionar la camara)
+//***************************************************************************
+
+void change_observer()
+{
+
+// posicion del observador
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+glTranslatef(0,0,-Observer_distance);
+glRotatef(Observer_angle_x,1,0,0);
+glRotatef(Observer_angle_y,0,1,0);
+}
+
+//**************************************************************************
+// Funcion que dibuja los ejes utilizando la primitiva grafica de lineas
+//***************************************************************************
+
+void draw_axis()
+{
+	
+glDisable(GL_LIGHTING);
+glLineWidth(2);
+glBegin(GL_LINES);
+// eje X, color rojo
+glColor3f(1,0,0);
+glVertex3f(-AXIS_SIZE,0,0);
+glVertex3f(AXIS_SIZE,0,0);
+// eje Y, color verde
+glColor3f(0,1,0);
+glVertex3f(0,-AXIS_SIZE,0);
+glVertex3f(0,AXIS_SIZE,0);
+// eje Z, color azul
+glColor3f(0,0,1);
+glVertex3f(0,0,-AXIS_SIZE);
+glVertex3f(0,0,AXIS_SIZE);
+glEnd();
+}
+
+
+
+void luces(float alfa, float beta){
+    GLfloat luz_ambiente [ ] = {0.3,0.3,0.3,1.0};
+    GLfloat luz1 [ ] = {1.0,1.0,1.0,1.0};
+    //GLfloat luz2 [ ] = {0.2,1.0,0.5,1.0};
+    GLfloat posicion1 [ ] = {50.0,50.0,0.0,1.0};
+    GLfloat posicion2 [ ] = {-50.0,50.0,0.0,1.0};
+
+    glLightfv (GL_LIGHT0, GL_AMBIENT, luz_ambiente);
+    glLightfv (GL_LIGHT1, GL_DIFFUSE, luz1);
+    glLightfv (GL_LIGHT1, GL_SPECULAR, luz1);
+
+    glPushMatrix();
+    glRotatef(alfa,0,1,0);
+    glTranslatef(0,beta,0);
+    glLightfv (GL_LIGHT1, GL_POSITION, posicion1);
+    glPopMatrix();
+   
+
+    //glLightfv (GL_LIGHT2, GL_DIFFUSE, luz2);
+    //glLightfv (GL_LIGHT2, GL_SPECULAR, luz2);
+    //glLightfv (GL_LIGHT2, GL_POSITION, posicion2);
+
+    glEnable(GL_LIGHT1);
+    //glEnable(GL_LIGHT2);
+
+
+
+}
+
+//**************************************************************************
+// Funcion que dibuja los objetos
+//****************************2***********************************************
+
+void draw_objects()
+{
+
+switch (t_objeto){
+	case CUBO: cubo.draw(modo,2,opcion);break;
+	case PIRAMIDE: piramide.draw(modo,2,opcion);break;
+        case OBJETO_PLY: ply.draw(modo,2,opcion);break;
+        case ESFERA: esfera.draw(modo,2,opcion);break;
+        case CONO: cono.draw(modo,2,opcion);break;
+        case CILINDRO: cilindro.draw(modo,2,opcion);break;
+        case GENERICO: copa.draw(modo,2,opcion);break;
+        case ARTICULADO: robot.draw(modo,2,opcion);break;
+	}
+	opcion=NO_OPTION;
+
+
+}
+
+
+//**************************************************************************
+//
+//***************************************************************************
+
+void draw(void)
+{
+
+clean_window();
+change_observer();
+luces(alfa,beta);
+draw_axis();
+draw_objects();
+glutSwapBuffers();
+}
+
+
+
+//***************************************************************************
+// Funcion llamada cuando se produce un cambio en el tamaño de la ventana
+//
+// el evento manda a la funcion:
+// nuevo ancho
+// nuevo alto
+//***************************************************************************
+
+void change_window_size(int Ancho1,int Alto1)
+{
+float Aspect_ratio;
+
+Aspect_ratio=(float) Alto1/(float )Ancho1;
+Size_y=Size_x*Aspect_ratio;
+change_projection();
+glViewport(0,0,Ancho1,Alto1);
+glutPostRedisplay();
+}
+
+
+//**********-o*****************************************************************
+// Funcion llamada cuando se aprieta una tecla normal
+//
+// el evento manda a la funcion:
+// codigo de la tecla
+// posicion x del raton
+// posicion y del raton
+//***************************************************************************
+
+void normal_key(unsigned char Tecla1,int x,int y)
+{
+switch (toupper(Tecla1)){
+	case 'Q':exit(0);
+	case '1':modo=POINTS;break;
+	case '2':modo=EDGES;break;
+	case '3':modo=SOLID;break;
+	case '4':modo=SOLID_CHESS;break;
+    case '5':modo=SOLID_ILUMINATED_FLAT;break;
+    case '6':modo=SOLID_ILUMINATED_GOURAUD;break;
+    case 'P':t_objeto=PIRAMIDE;break;
+    case 'C':t_objeto=CUBO;break;
+    case 'O':t_objeto=OBJETO_PLY;break;	
+    case 'A':t_objeto=ESFERA;break;
+    case 'S':t_objeto=CILINDRO;break;
+    case 'D':t_objeto=CONO;break;
+    case 'X':opcion=COLORPICKER;break;
+    case 'G':t_objeto=GENERICO;break;
+    case 'T':t_objeto=ARTICULADO;break;
+
+    //Movimientos
+    case 'W': robot.giro_cabeza += v_giro_cabeza; break;
+    case 'E': robot.giro_cabeza -= v_giro_cabeza; break;
+    case 'H': robot.giro_mano_izquierda += v_giro_brazos; break;
+    case 'J': robot.giro_mano_izquierda -= v_giro_brazos; break;
+    case 'K': robot.giro_mano_derecha += v_giro_brazos; break;
+    case 'L': robot.giro_mano_derecha -= v_giro_brazos; break;
+    case 'V': robot.giro_pie_derecho -= v_giro_piernas; break;
+    case 'B': robot.giro_pie_derecho += v_giro_piernas; break;
+    case 'N': robot.giro_pie_izquierdo -= v_giro_piernas; break;
+    case 'M': robot.giro_pie_izquierdo += v_giro_piernas; break; 
+  
+	}
+glutPostRedisplay();
+}
+
+//***************************************************************************
+// Funcion l-olamada cuando se aprieta una tecla especial
+//
+// el evento manda a la funcion:
+// codigo de la tecla
+// posicion x del raton
+// posicion y del raton
+
+//***************************************************************************
+
+void special_key(int Tecla1,int x,int y)
+{
+
+switch (Tecla1){
+	case GLUT_KEY_LEFT:Observer_angle_y--;break;
+	case GLUT_KEY_RIGHT:Observer_angle_y++;break;
+	case GLUT_KEY_UP:Observer_angle_x--;break;
+	case GLUT_KEY_DOWN:Observer_angle_x++;break;
+	case GLUT_KEY_PAGE_UP:Observer_distance*=1.2;break;
+	case GLUT_KEY_PAGE_DOWN:Observer_distance/=1.2;break;
+        case GLUT_KEY_F1:tanque.giro_tubo+=1;
+                         if (tanque.giro_tubo>tanque.giro_tubo_max) tanque.giro_tubo=tanque.giro_tubo_max;
+                         break;
+        case GLUT_KEY_F2:tanque.giro_tubo-=1;
+                         if (tanque.giro_tubo<tanque.giro_tubo_min) tanque.giro_tubo=tanque.giro_tubo_min;
+                         break;break;
+        case GLUT_KEY_F3:tanque.giro_torreta+=5;break;
+        case GLUT_KEY_F4:tanque.giro_torreta-=5;break;
+        case GLUT_KEY_F5:alfa+=5;break;
+        case GLUT_KEY_F6:alfa-=5;break;
+        case GLUT_KEY_F7:beta+=5;break;
+        case GLUT_KEY_F8:beta-=5;break;
+
+	}
+glutPostRedisplay();
+}
+
+
+
+//***************************************************************************
+// Funcion de incializacion
+//***************************************************************************
+
+void initialize(void)
+{
+
+// se inicalizan la ventana y los planos de corte
+Size_x=0.5;
+Size_y=0.5;
+Front_plane=1;
+Back_plane=1000;
+
+// se incia la posicion del observador, en el eje z
+Observer_distance=4*Front_plane;
+Observer_angle_x=0;
+Observer_angle_y=0;
+
+// se indica cua*ply1l sera el color para limpiar la ventana	(r,v,a,al)
+// blanco=(1,1,1,1) rojo=(1,0,0,1), ...
+glClearColor(1,1,1,1);
+
+// se habilita el z-bufer
+glEnable(GL_DEPTH_TEST);
+change_projection();
+glViewport(0,0,Window_width,Window_high);
+
+
+
+}
+
+
+//***************************************************************************
+// Programa principal
+//
+// Se encarga de iniciar la ventana, asignar las funciones e comenzar el
+// bucle de eventos
+//***************************************************************************
+
+
+int main(int argc, char *argv[] )
+{
+ 
+
+
+
+// se llama a la inicialización de glut
+glutInit(&argc, argv);
+
+// se indica las caracteristicas que se desean para la visualización con OpenGL
+// Las posibilidades son:
+// GLUT_SIMPLE -> memoria de imagen simple
+// GLUT_DOUBLE -> memoria de imagen doble
+// GLUT_INDEX -> memoria de imagen con color indizado
+// GLUT_RGB -> memoria de imagen con componentes rojo, verde y azul para cada pixel
+// GLUT_RGBA -> memoria de imagen con componentes rojo, verde, azul y alfa para cada pixel
+// GLUT_DEPTH -> memoria de profundidad o z-bufer
+// GLUT_STENCIL -> memoria de estarcido_rotation Rotation;
+glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+// posicion de la esquina inferior izquierdad de la ventana
+glutInitWindowPosition(Window_x,Window_y);
+
+// tamaño de la ventana (ancho y alto)
+glutInitWindowSize(Window_width,Window_high);
+
+// llamada para crear la ventana, indicando el titulo (no se visualiza hasta que se llama
+// al bucle de eventos)
+glutCreateWindow("PRACTICA - 2: Fernando Lojano");
+
+// asignación de la funcion llamada "dibujar" al evento de dibujo
+glutDisplayFunc(draw);
+// asignación de la funcion llamada "change_window_size" al evento correspondiente
+glutReshapeFunc(change_window_size);
+// asignación de la funcion llamada "normal_key" al evento correspondiente
+glutKeyboardFunc(normal_key);
+// asignación de la funcion llamada "tecla_Especial" al evento correspondiente
+glutSpecialFunc(special_key);
+
+// funcion de inicialización
+initialize();
+
+// creación del objeto ply
+ply.parametros(argv[1]);
+
+//ply1 = new _objeto_ply(argv[1]);
+
+// inicio del bucle de eventos
+glutMainLoop();
+return 0;
+}
